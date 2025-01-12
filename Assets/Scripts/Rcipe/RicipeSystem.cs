a7929483452ffdc48299a6f023c82939702e53cc
47StanyS <stanyslavalokhin@gmail.com>
2025-01-12 11:47:14 +0200

    не впевнений що правильно

--- Assets/Scripts/Rcipe/RicipeSystem.cs
+++ Assets/Scripts/Rcipe/RicipeSystem.cs
@@ -16,4 +16,27 @@ public class RicipeSystem : MonoBehaviour
         _collectedIngredients[(int)_ingredient]++;
         Debug.Log($"Add{_ingredient} all{_collectedIngredients[(int)_ingredient]}");
     }
+
+    public void CraftRecipe(string _recipeName)
+    {
+        switch (_recipeName)
+        {
+            case "CircleTriangle":
+                if (_collectedIngredients[(int)IngredientType.Capsule] >= 2 &&
+                    _collectedIngredients[(int)IngredientType.Circle] >= 1 &&
+                    _collectedIngredients[(int)IngredientType.Triangle] >= 1)
+                {
+                    _collectedIngredients[(int)IngredientType.Capsule] -= 2;
+                    _collectedIngredients[(int)IngredientType.Circle] -= 1;
+                    _collectedIngredients[(int)IngredientType.Triangle] -= 1;
+
+                    Debug.Log("nice,you have soup");
+                }
+                else
+                {
+                    Debug.Log("missing ingredients");
+                }
+                break;
+        }
+    }
 }
